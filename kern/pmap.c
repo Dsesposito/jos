d6e555ab0e5fedf4410f7e46eb6506118ff6252e
lucas <lucas.etcheverry28@gmail.com>
2018-10-10 21:24:23 -0300

    Version large pages finalizado

--- kern/pmap.c
+++ kern/pmap.c
@@ -196,7 +196,7 @@ mem_init(void)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
 
-	boot_map_region(kern_pgdir, UPAGES, PTSIZE, PADDR(pages), PTE_U);
+	boot_map_region(kern_pgdir, UPAGES, (npages * sizeof(struct PageInfo)), PADDR(pages), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
