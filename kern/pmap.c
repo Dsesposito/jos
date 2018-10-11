3685ba02d5802c870891bb9c3eed99d08b639b3a
dsesposito <diego92.esposito@gmail.com>
2018-10-11 18:52:13 -0300

    Actualización de boot map region para el arreglo de pages.

--- kern/pmap.c
+++ kern/pmap.c
@@ -195,8 +195,8 @@ mem_init(void)
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
-
-	boot_map_region(kern_pgdir, UPAGES, (npages * sizeof(struct PageInfo)), PADDR(pages), PTE_U);
+    size_t pageSize = ROUNDUP((npages * sizeof(struct PageInfo)), PGSIZE);
+	boot_map_region(kern_pgdir, UPAGES, pageSize, PADDR(pages), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
