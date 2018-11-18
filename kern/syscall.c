260b7176d14075b0dc8606196d50d3f5d0332ae0
ferkreps <ferkreps@gmail.com>
2018-11-18 17:06:59 -0300

    dumbfork : OK

--- kern/syscall.c
+++ kern/syscall.c
@@ -355,6 +355,16 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 	// LAB 3: Your code here.
 
 	switch (syscallno) {
+  case SYS_exofork:
+    return sys_exofork();
+  case SYS_env_set_status:
+      return sys_env_set_status(a1, a2);
+  case SYS_page_alloc:
+      return sys_page_alloc(a1, (void *)a2, a3);
+  case SYS_page_map:
+      return sys_page_map(a1, (void *)a2, a3, (void *)a4, a5);
+  case SYS_page_unmap:
+      return sys_page_unmap(a1, (void *)a2);                
 	case SYS_cputs:
 		sys_cputs((char *) a1, a2);
 		return 0;
